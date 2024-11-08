c5bd9d45dac4b2ae0c99748ccfbc99f860f30d1e
aegis945 <artiom.lekincev@gmail.com>
2024-11-08 14:07:08 +0200

    Add incremental likes and enforce unique constraints for Follow model

--- network/models.py
+++ network/models.py
@@ -9,7 +9,7 @@ class User(AbstractUser):
 class Post(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
-    likes = models.ManyToManyField(User, related_name="liked_posts", blank=True)
+    likes = models.PositiveIntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
     
     class Meta:
@@ -21,12 +21,19 @@ class Follow(models.Model):
     following = models.ForeignKey(User, on_delete=models.CASCADE, related_name="followed_by")
     created_at = models.DateTimeField(auto_now_add=True)
     
+    class Meta:
+        unique_together = ("follower", "following")
+    
     
 class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     content = models.TextField()
-    likes = models.ManyToManyField(User, related_name="liked_comments", blank=True)
+    likes = models.PositiveIntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
     
-    
+    class Meta:
+        ordering = ["-created_at"],
+        indexes = [
+            models.Index(fields=["created_at"])
+        ]
