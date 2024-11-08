1593b4e9ec51887f09dee06e7e006a18b2a8c301
aegis945 <artiom.lekincev@gmail.com>
2024-11-08 15:53:52 +0200

    make the app more mobile responsive, adjusted layout for mobile devices

--- network/forms.py
+++ network/forms.py
@@ -2,15 +2,15 @@ from django import forms
 from .models import Post
 
 class NewPostForm(forms.ModelForm):
-    class Meta:
-        model = Post
-        fields = ['content']
-        
     content = forms.CharField(
         widget=forms.Textarea(attrs={
             'placeholder': "Write a new post...",
-            'class': 'form-control',
+            'class': 'form-control mt-3',
             'rows': '4',
         }),
         label="What's on your mind?"
     )
+    
+    class Meta:
+        model = Post
+        fields = ['content']
