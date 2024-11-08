c4e18affe7d74e60eda8e73e5ed374f5b9de6149
aegis945 <artiom.lekincev@gmail.com>
2024-11-08 02:57:46 +0200

    create models

--- network/models.py
+++ network/models.py
@@ -4,3 +4,26 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+    
+    
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    likes = models.ManyToManyField(User, related_name="liked_posts", blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    
+class Follow(models.Model):
+    follower = models.ForeignKey(User, on_delete=models.CASCADE, related_name="following")
+    following = models.ForeignKey(User, on_delete=models.CASCADE, related_name="followed_by")
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    content = models.TextField()
+    likes = models.ManyToManyField(User, related_name="liked_comments", blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    
